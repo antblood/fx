e96af99ec928bd707d6fdd869eea6bd798b149bc
Neeraj Battan <nbattan@rippling.com>
2025-02-06 14:22:46 +0530

    more updates in the code

--- problem/pkg/shared_worker/worker.go
+++ problem/pkg/shared_worker/worker.go
@@ -19,9 +19,17 @@ type RegisterActivitiesOut struct {
 }
 
 func RegisterActivities(params WorkerParams) RegisterActivitiesOut {
+
+	// ===================
+	// I want you to keep this part of the code as is
 	params.Worker.RegisterActivity("activity1")
 	params.Worker.RegisterActivity("activity2")
+	// till here
+	// ===================
 
+	// if you just add "activity1" and "activity2" here, it will work
+	// but if I add more activities, it will break
+	// because new activities will need to be added here manually
 	return RegisterActivitiesOut{
 		Activities: []string{},
 	}
